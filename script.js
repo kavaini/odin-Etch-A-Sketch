62e39acd3be3b67d43011f6b30d3f19e9cacf27b
kavaini <karolina.vainilkaite@gmail.com>
2022-09-07 18:49:29 +0300

    feat: :art: add board on js file

---
+++ script.js
@@ -0,0 +1,16 @@
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.getElementsByClassName.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.getElementsByClassName.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "green";
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+
+populateBoard(16);
